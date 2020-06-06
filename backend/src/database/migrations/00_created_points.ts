b045da941f642460feefc56e02d7e20e37b0f434
ItsJuniorDias <juniordias_@live.com>
2020-06-05 23:10:20 -0300

    Create migrations application

--- backend/src/database/migrations/00_created_points.ts
+++ backend/src/database/migrations/00_created_points.ts
@@ -1,7 +1,7 @@
 import Knex from 'knex';
 
 export async function up(knex: Knex){
-  knex.schema.createTable('points', table => {
+   return knex.schema.createTable('points', table => {
     table.increments('id').primary();
     table.string('image').notNullable();
     table.string('name').notNullable();
