a8aa2d32a365b33113ff21688f3b032975be761b
ItsJuniorDias <juniordias_@live.com>
2020-06-07 20:51:12 -0300

    Filter Query

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,25 +2,18 @@ import express from 'express';
 import knex from './database/connection';
 
 import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';  
 
 const routes = express.Router();
 const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async  (resquest, response ) => {
-  const items =  await knex('items').select('*');
+//routes Items
+routes.get('/items',  itemsController.index);
+
+//routes Points
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index); 
+routes.get('/points/:id', pointsController.show);  
   
-
-  const serializedItems = items.map(item => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `https://localhost:3333/uploads/${item.image}`,
-    }
-  })
-
-  return response.json(serializedItems)
-});
-
-routes.post('/points', pointsController.create); 
-
-export default routes;
+export default routes;  
