dbe7a8b953687d5b59a85d3008fe801a07370a5f
ItsJuniorDias <juniordias_@live.com>
2020-06-11 22:39:05 -0300

    Install react router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello World </h1>
-    </div>
+     <Routes/>
   );
 }
 
